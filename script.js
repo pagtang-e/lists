ee4d450d559c1fac7fe842dbb83c4bf14c551064
Zofia Mamrowicz <117534066+pagtang-e@users.noreply.github.com>
2023-06-20 22:45:16 +0200

    added all basic functionalities

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-let tx = document.getElementsByTagName("textarea");
+let tx;
 let main = document.querySelector("main");
 let add_first= document.querySelector("#add_first");
+let liDeleteBtn;
+let articleDeleteBtn;
+let liAddBtn;
+let AddArticleDivCreated;
 
-textareaResize()
+
 add_first.addEventListener("click", ()=>{
     add_first.style.display='none';
 add_List();
-textareaResize();
+addNewArticle()
 
-   
 });
-function liDelete(){
-    
-}
+
+
+
 
 function add_List(){
 let article = document.createElement("article");
@@ -38,27 +41,46 @@ li.append(textarea);
 tx = document.getElementsByTagName("textarea");
 
 let bin_btn = document.createElement('button')
-bin_btn.setAttribute('class','bin')
-bin_btn.setAttribute('onClick',"liDelete()")
+bin_btn.setAttribute('class','bin liDel')
 li.append(bin_btn)
+liDeleteBtn = document.querySelectorAll('.liDel')
+liDeleteBtn = Array.from(liDeleteBtn)
+
+
+
 
 let nav = document.createElement('nav');
 article.append(nav)
 
 let add_btn = document.createElement('button');
-add_btn.setAttribute('class','add');
+add_btn.setAttribute('class','add liAdd');
 nav.append(add_btn)
+liAddBtn = document.querySelectorAll('.liAdd')
+liAddBtn = Array.from(liAddBtn)
+
+
+
 
 let bin_btn2 = document.createElement('button')
-bin_btn2.setAttribute('class','bin')
+bin_btn2.setAttribute('class','bin articleDelete')
 nav.append(bin_btn2);
+articleDeleteBtn = document.querySelectorAll('.articleDelete');
+articleDeleteBtn = Array.from(articleDeleteBtn);
+
+
+
+ArticleDelete();
+liAdd()
+textareaResize();
+LiDelete();
 
 }
 
 function textareaResize(){
+  tx = document.getElementsByTagName("textarea");
 for (let i = 0; i < tx.length; i++) {
 
-  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  tx[i].setAttribute("style", "overflow-y:hidden;");
   tx[i].addEventListener("input", OnInput, false);
 }
 
@@ -67,3 +89,74 @@ function OnInput() {
   this.style.height = 0;
   this.style.height = (this.scrollHeight) + "px";
 }}
+function LiDelete(){
+  
+let latest = liDeleteBtn.slice(-1)[0];
+  latest.addEventListener('click', ()=>{
+
+   latest.parentElement.remove();
+    
+    
+  })
+}
+function ArticleDelete(){
+  let latest = articleDeleteBtn.slice(-1)[0];
+    latest.addEventListener('click', ()=>{
+     
+  
+      latest.parentElement.parentElement.remove();
+      
+    })
+}
+  
+function liAdd(){
+  
+  
+  let latest = liAddBtn.slice(-1)[0];
+   
+  latest.addEventListener('click', ()=>{
+      
+     
+    
+      let li = document.createElement('li');
+      latest.parentElement.previousElementSibling.append(li)
+     
+     let textarea = document.createElement('textarea');
+     textarea.setAttribute('placeholder','wpisz zadanie')
+     li.append(textarea);
+     tx = document.getElementsByTagName("textarea");
+     textareaResize();
+     
+     let bin_btn = document.createElement('button')
+     bin_btn.setAttribute('class','bin liDel')
+     li.append(bin_btn)
+     liDeleteBtn = document.querySelectorAll('.liDel')
+     liDeleteBtn = Array.from(liDeleteBtn)
+     LiDelete();
+  
+  })
+    
+    }
+
+
+function addNewArticle(){
+
+  let AddArticleDiv = document.createElement('div');
+AddArticleDiv.setAttribute('class', 'AddArticleDiv');
+main.append(AddArticleDiv);
+AddArticleDivCreated = document.querySelector('.AddArticleDiv');
+AddArticleDivCreated.addEventListener('click', ()=>{
+  add_List();
+  main.append(AddArticleDivCreated)
+
+})
+
+let AddArticlePlusOne = document.createElement('div');
+AddArticlePlusOne.setAttribute('class','cross one AddArticle');
+AddArticleDiv.append(AddArticlePlusOne);
+
+let AddArticlePlusTwo = document.createElement('div');
+AddArticlePlusTwo.setAttribute('class','cross two AddArticle');
+AddArticleDiv.append(AddArticlePlusTwo);
+
+}
